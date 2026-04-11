dfee9afd9f8c125ce657e5806932f9ebed43c111
alison <luoxiaoqing44@gmail.com>
2026-04-11 18:15:35 +0800

    refactor: encapsulate grid creation into a function and improve logic

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 const container = document.getElementById("container");
-let num = 4;
-for(let x = 0; x < num; x++){
-    const row = document.createElement("div"); 
-    row.classList.add("row");
-    for(let j = 0; j < num; j++){
-        const column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
+const btn = document.querySelector("button");
+
+createGrid(16);
+function createGrid(size){
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
     }
-    container.appendChild(row);
+    for(let x = 0; x < size; x++){
+        const row = document.createElement("div"); 
+        row.classList.add("row");
+        for(let j = 0; j < size; j++){
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+        container.appendChild(row);
+    }
 }
 
-container.addEventListener("mouseover",(e) => {
-    const r = Math.floor(Math.random()*256);
-    const g = Math.floor(Math.random()*256);
-    const b = Math.floor(Math.random()*256);
-    e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
-})
 
-const btn = document.querySelector("button");
+container.addEventListener("mouseover",(e) => {
+    if (e.target.classList.contains("column")){
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+    
+});
+
+
 btn.addEventListener("click",() => {
-    let userInput = prompt("enter a number between 1 and 100:",4);
-    if (userInput >=1 && userInput <= 100){
-        while (container.firstChild){
-            container.removeChild(container.firstChild);
-        }
-        for(let x = 0; x < userInput; x++){
-            const row = document.createElement("div"); 
-            row.classList.add("row");
-            for(let j = 0; j < userInput; j++){
-                const column = document.createElement("div");
-                column.classList.add("column");
-                row.appendChild(column);
-            }
-            container.appendChild(row);
-        }
+    let userInput = prompt("Enter a number (1-100):",16);
+    let val = parseInt(userInput);
+    if (val >=1 && val <= 100){
+        createGrid(val);
     } else {
         alert("please enter a valid number.");
     }
     
-})
+});
 
 
